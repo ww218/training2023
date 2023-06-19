133dc8afdf12a218e16d469cc3f139703e0e2dbf
ww218 <g8wlc681208@gmail.com>
2023-06-19 20:09:03 +0800

    trainingD-1_clock

--- Assets/scripts/Clock.cs
+++ Assets/scripts/Clock.cs
@@ -1,6 +1,15 @@
+using System;
 using UnityEngine;
 public class Clock : MonoBehaviour{
+	const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+	[SerializeField]
+	Transform hoursPivot, minutesPivot, secondsPivot;
 
-    [SerializeField]
-    Transform hoursPivot;
+	void Update()
+	{
+		var time = DateTime.Now;
+		hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * time.Hour);
+		minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * time.Minute);
+		secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * time.Second);
+	}
 }
